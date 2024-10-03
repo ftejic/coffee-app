b6a7ace23bacddc20835be563ea76eafd50c9d41
Filip Tejic <ficatejic@gmail.com>
2024-10-03 21:10:19 +0200

    commit

---
+++ my-app/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'counter', component: CounterComponent },
+];
